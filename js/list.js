70b45f89cb02fc0e314f1a40d03d30a76f377076
jeeseongmin <peration0422@exntu.com>
2023-11-09 16:36:46 +0900

    feat : interactive list component UI 개발

--- js/list.js
+++ js/list.js
@@ -1,9 +1,10 @@
 /**
+ * Nested List
  * 1. arrow icon 변경 - text를 expand_more | expand_less
  * 2. nextLi 내 toggle-hidden class toggle 처리
  */
-let haha = document.getElementById("toggle-li");
-haha.addEventListener("click", function (event) {
+let toggleLi = document.getElementById("toggle-li");
+toggleLi.addEventListener("click", function (event) {
   let li, nextLi, icon;
   if (event.target.tagName === "LI") li = event.target;
   else li = event.target.parentNode;
@@ -15,4 +16,29 @@ haha.addEventListener("click", function (event) {
   else icon.innerText = "expand_more";
   // nested-li toggle 처리
   nextLi.classList.toggle("toggle-hidden");
+});
+
+/**
+ * Interactive components
+ * 1. id = interactive-list 하위 list-cmponent들에 dense toggle
+ * 2. secondary
+ */
+
+// Enable Dense
+let denseCheckbox = document.getElementById("enable-dense");
+denseCheckbox.addEventListener("click", function (event) {
+  let listComponents = document.querySelectorAll("#interactive-component");
+  listComponents.forEach((item) => {
+    item.classList.toggle("dense");
+  });
+});
+
+// Enable secondary text
+let secondaryCheckbox = document.getElementById("enable-secondary-text");
+secondaryCheckbox.addEventListener("click", function (event) {
+  console.log(event.target.checked);
+  let secondaryTextComponents = document.querySelectorAll("#secondary-text");
+  secondaryTextComponents.forEach((item) => {
+    item.classList.toggle("hidden");
+  });
 });
